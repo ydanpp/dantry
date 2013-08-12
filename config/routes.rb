0454e32240ac3b6086324c331b30d0d7d35ac617
DanH <ydanpp@gmail.com>
2013-08-12 23:49:20 +0800

    add home and about pages,edit layouts files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Dantry::Application.routes.draw do
+  get 'about'=>'pages#about'
+  root :to => 'Pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
